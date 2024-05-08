2d6fc50c2a360106ce62702e1c0869a3a57b0808
selim021 <brahamselim09@gmail.com>
2024-05-08 11:17:17 +0100

    Refactor Table component

--- client/src/pages/Table.tsx
+++ client/src/pages/Table.tsx
@@ -1,6 +1,4 @@
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
-// import TableOne from '../components/Tables/TableOne';
-import TableThree from '../components/Tables/TableThree';
 import TableTwo from '../components/Tables/TableTwo';
 import DefaultLayout from '../layout/DefaultLayout';
 
@@ -10,9 +8,9 @@ const Table = () => {
       <Breadcrumb pageName="Table" />
 
       <div className="flex flex-col gap-10">
-        {/* <TableOne /> */}
+       
         <TableTwo />
-        <TableThree />
+        
       </div>
     </DefaultLayout>
   );
