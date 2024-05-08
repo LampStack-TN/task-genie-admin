d5a83a633a3862be65c2b85c06cac97b0ca12955
ahmedGDI <guedria.guedri@gmail.com>
2024-05-08 19:02:22 +0100

    remove calendar from app , index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
 import SignIn from './pages/Authentication/SignIn';
-import Calendar from './pages/Calendar';
 import Main from './pages/Dashboard/Main';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
@@ -37,15 +35,6 @@ function App() {
             <>
               <PageTitle title=" Dashboard | TaskGenie" />
               <Main />
-            </>
-          }
-        />
-        <Route
-          path="/calendar"
-          element={
-            <>
-              <PageTitle title="Calendar | TaskGenie" />
-              <Calendar />
             </>
           }
         />
