4ee3dedc4c6671847cdf52bf9dbb496195a252d9
selim021 <brahamselim09@gmail.com>
2024-05-08 09:44:08 +0100

    fetch clients to table two

--- client/src/components/Tables/TableTwo.tsx
+++ client/src/components/Tables/TableTwo.tsx
@@ -1,38 +1,52 @@
-import { Product } from '../../types/product';
-
-
-const productData: Product[] = [
- 
-];
+import React, { useEffect, useState } from 'react';
+import { Clients } from '../../types/Clients';
 
 const TableTwo = () => {
+  const [clients, setClients] = useState<Clients[]>([]);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3000/api/admin/clients',
+        );
+        const data = await response.json();
+        setClients(data);
+      } catch (error) {
+        console.error('Failed to fetch Clients:', error);
+      }
+    };
+
+    fetchClients();
+  }, []);
+
   return (
-    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark" style={{ maxHeight: '500px', overflowY: 'auto' }}>
       <div className="py-6 px-4 md:px-6 xl:px-7.5">
         <h4 className="text-xl font-semibold text-black dark:text-white">
-          Top Products
+          Clients
         </h4>
       </div>
-
+  
       <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
         <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
+          <p className="font-medium">fullName</p>
         </div>
         <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Category</p>
+          <p className="font-medium">email</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Price</p>
+          <p className="font-medium">role</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Sold</p>
+          <p className="font-medium">address</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Profit</p>
+          <p className="font-medium">zipcode</p>
         </div>
       </div>
-
-      {productData.map((product, key) => (
+  
+      {clients.map((client, key) => (
         <div
           className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
           key={key}
@@ -40,33 +54,34 @@ const TableTwo = () => {
           <div className="col-span-3 flex items-center">
             <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
               <div className="h-12.5 w-15 rounded-md">
-                <img src={product.image} alt="Product" />
+                <img src={client.avatar} alt="Product" />
               </div>
               <p className="text-sm text-black dark:text-white">
-                {product.name}
+                {client.fullName}
               </p>
             </div>
           </div>
           <div className="col-span-2 hidden items-center sm:flex">
             <p className="text-sm text-black dark:text-white">
-              {product.category}
+              {client.email}
             </p>
           </div>
           <div className="col-span-1 flex items-center">
             <p className="text-sm text-black dark:text-white">
-              ${product.price}
+              {client.role}
             </p>
           </div>
           <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black dark:text-white">{product.sold}</p>
+            <p className="text-sm text-black dark:text-white">{client.address}</p>
           </div>
           <div className="col-span-1 flex items-center">
-            <p className="text-sm text-meta-3">${product.profit}</p>
+            <p className="text-sm text-meta-3">${client.zipcode}</p>
           </div>
         </div>
       ))}
     </div>
   );
+  
 };
 
 export default TableTwo;
