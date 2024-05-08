de3f4cecb638bd268dfe0b2acc22b6ec12c1f7df
ahmedGDI <guedria.guedri@gmail.com>
2024-05-08 15:15:55 +0100

    implement authentication process from back to front

--- client/src/utils/checkAuthentication.ts
+++ client/src/utils/checkAuthentication.ts
@@ -2,11 +2,15 @@ import { ApiClient } from "./api";
 const checkAuthentication = async () => {
   if (await localStorage.getItem("token")) {
     try {
-      const { } = await ApiClient().get("admin/verify-token");
-      
+      const response = await ApiClient().get("auth/verify-token");
+      if (response.status === 200) {
+      } else {
+        await localStorage.removeItem("token");
+      }
     } catch (error) {
+      console.error("Error verifying token:", error);
+      await localStorage.removeItem("token");
       
-      await localStorage.clear()
     }
   }
 };
