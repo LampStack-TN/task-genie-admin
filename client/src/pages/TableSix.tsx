a5db60e01de311687430fe61cdd7173dcbc139c5
selim021 <brahamselim09@gmail.com>
2024-05-17 01:02:35 +0100

    adjust ProfileDetails for TaskDetails

---
+++ client/src/pages/TableSix.tsx
@@ -0,0 +1,19 @@
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+import TaskDetails from '../components/Tables/TaskDetails';
+import DefaultLayout from '../layout/DefaultLayout';
+
+const TableFive = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Clients Table" />
+
+      <div className="flex flex-col gap-10">
+       
+        <TaskDetails />
+        
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default TableFive;
